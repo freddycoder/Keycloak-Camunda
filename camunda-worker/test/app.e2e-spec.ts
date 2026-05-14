41477d67bbdcc7f5f7aa6db76c1c682e4cd8f670
Frédéric Jacques <gary.bob88@gmail.com>
2026-05-14 15:21:38 -0400

    fix test

--- camunda-worker/test/app.e2e-spec.ts
+++ camunda-worker/test/app.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request.agent(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
